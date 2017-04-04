8aa34f1902de84b1e290230552bf3ce7f7dc0b5c
Marco Damaceno <maadamaceno@gmail.com>
2017-04-04 13:55:16 -0300

    Other page

---
+++ src/components/Other.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.outra
+    h1 {{ msg }}
+    router-link(to="/") Hello
+</template>
+
+<script>
+export default {
+  name: 'outra-pagina',
+  data () {
+    return {
+      msg: 'Outra Página'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
